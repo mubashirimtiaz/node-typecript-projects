c0c242198e9e53dedf2829ca09ee2c798a804920
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 00:38:24 +0500

    exercise 15 completed

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+const host = 'Mubashir';
+
 const guests = ['John', 'Jane', 'Mary'];
 
-guests.forEach((guest) => {
-  console.log(
-    `Hello ${guest}, Hope you are doing well. I am arranging a dinner on 15th of this month and you are invited .🥳`
-  );
-});
+const guestWhoCantMakeIt = 'Jane';
+
+const inviteToDinner = () => {
+  let loop = false;
+  guests.forEach((guest) => {
+    console.log(
+      `Hello ${guest}, Hope you are doing well. I am arranging a dinner on 15th of this month and you are invited .🥳`
+    );
+    if (guest === guestWhoCantMakeIt) {
+      console.log(`Sorry ${host}, ${guest} here, I can't come. 😔`);
+      guests.splice(guests.indexOf(guest), 1, 'Sara');
+      loop = true;
+    }
+  });
+  if (loop) {
+    inviteToDinner();
+  }
+};
+
+inviteToDinner();
