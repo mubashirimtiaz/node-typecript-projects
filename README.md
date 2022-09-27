13cf94f7eef4565a7d1ca08a2a3f022f8b5f9d8d
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 03:54:43 +0500

    exercise 31 completed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # node-javascript-projects
 
-Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+
+- If the list is empty, print the message We need to find some users!
+
+- Remove all of the usernames from your array, and make sure the correct message is printed.
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const users = ['admin', 'hassan', 'raza', 'sara', 'waleed'];
+const users = [];
 
-users.forEach((user) => {
-  if (user === 'admin') {
-    console.log('Hello admin, would you like to see a status report?');
-  } else {
-    console.log(`Hello ${user}, thank you for logging in again.`);
-  }
-});
+if (users.length === 0) {
+  console.log('We need to find some users!');
+} else {
+  users.forEach((user) => {
+    if (user === 'admin') {
+      console.log('Hello admin, would you like to see a status report?');
+    } else {
+      console.log(`Hello ${user}, thank you for logging in again.`);
+    }
+  });
+}
