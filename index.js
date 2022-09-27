4e927ed64fdfaa72bab95e4eb451fb436e77e77f
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 00:11:29 +0500

    exercise 10 completed

---
+++ index.js
@@ -0,0 +1,13 @@
+//This program will add any two numbers and return their sum
+function add(a, b) {
+  return a + b;
+}
+
+//This program will take 3 args and if any of three greater than 2 it will return true else false
+function greaterThan2(a, b, c) {
+  if (a > 2 && b > 2 && c > 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
