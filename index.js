1c52513aa18a3e2b4461febed4841c9ecab410c6
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-27 23:50:24 +0500

    exercise 6 completed

---
+++ index.js
@@ -0,0 +1,13 @@
+const name1 = `Mu  
+              bas  hir`;
+
+const name2 = '  Mubashir Ali   ';
+
+console.log('name1:', name1);
+console.log('name2:', name2);
+
+const trimmedName1 = name1.replace(/\s*/g, '');
+const trimmedName2 = name2.trim();
+
+console.log('trimmedName1:', trimmedName1);
+console.log('trimmedName2:', trimmedName2);
