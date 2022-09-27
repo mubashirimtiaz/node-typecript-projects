82ade4d815029a16b92e468d1f5872c5b96413f4
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 01:42:46 +0500

    exercise 19 completed

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ guests.forEach((guest) => {
     `Hello ${guest}, Hope you are doing well. I am arranging a dinner on 15th of this month and you are invited .🥳`
   );
 });
+
+console.log(`Hurrah! I have invited ${guests.length} guests`);
