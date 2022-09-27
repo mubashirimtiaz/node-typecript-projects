38fe9c22979a75335a10a7f58784bb0c4fd8e086
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 01:40:06 +0500

    exercise 18 completed

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-const citiesToVisit = [
-  'New York',
-  'London',
-  'Barcelona',
-  'Dubai',
-  'Tokyo',
-  'Hong Kong',
-];
+let citiesToVisit = ['london', 'barcelona', 'dubai', 'tokyo', 'russia'];
+
+console.log(citiesToVisit);
+
+//ordered alphabetically
+
+const sortedCitiesToVisit = citiesToVisit.sort();
+
+console.log(sortedCitiesToVisit);
+
+console.log(citiesToVisit);
+
+//ordered alphabetically in reverse
+
+const reverseSortedCitiesToVisit = citiesToVisit.sort().reverse();
+
+console.log(reverseSortedCitiesToVisit);
+
+console.log(citiesToVisit);
+
+//reverse actual array
+
+citiesToVisit = citiesToVisit.reverse();
+
+console.log(citiesToVisit);
+
+//reverse actual array
+
+citiesToVisit = citiesToVisit.reverse();
+
+console.log(citiesToVisit);
+
+//sort actual array
+
+citiesToVisit = citiesToVisit.sort();
+
+console.log(citiesToVisit);
+
+//sort actual array in reverse
+
+citiesToVisit = citiesToVisit.reverse();
+
+console.log(citiesToVisit);
