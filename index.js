46745b1eab64b4b71f697e1c0ad3c323c41c8068
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 00:25:19 +0500

    exercise 14 completed

---
+++ index.js
@@ -0,0 +1,7 @@
+const guests = ['John', 'Jane', 'Mary'];
+
+guests.forEach((guest) => {
+  console.log(
+    `Hello ${guest}, Hope you are doing well. I am arranging a dinner on 15th of this month and you are invited .🥳`
+  );
+});
