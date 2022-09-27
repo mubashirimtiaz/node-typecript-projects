082411c1660fb3421f1753a33f94501730ccc5d6
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 04:31:45 +0500

    exercise 35 completed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const pizza_flavors = ['Pepperoni', 'Sausage', 'Cheese'];
+const animals = ['Cat', 'Dog', 'Rabbit'];
 
-for (let index = 0; index < pizza_flavors.length; index++) {
-  // console.log(pizza_flavors[index]);
-  console.log(`I like ${pizza_flavors[index]} pizza`);
+for (let index = 0; index < animals.length; index++) {
+  // console.log(animals[index]);
+  console.log(`A ${animals[index]} would make a great pet`);
 }
 
-console.log('I really love pizza!');
+console.log('All these animals would make a great pet!');
