49099a08f46b9ea1f462ff746714491fb493bf7f
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 04:39:13 +0500

    exercise 37 completed

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const make_shirt = (size, text) => {
+const make_shirt = (size = 'large', text = 'I love JavaScript') => {
+  if (size === 'large' || size === 'medium') {
+    return `A ${size} size shirt with the text "I love JavaScript"`;
+  }
   return `A ${size} size shirt with the text "${text}"`;
 };
-
-const order_summary = make_shirt('large', 'I love JS!');
+const order_summary = make_shirt('small', 'I love JS!');
 
 console.log(order_summary);
