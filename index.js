c8914696002ec10215f6a19535bca792f39bc5a0
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 04:28:37 +0500

    exercise 34 completed

---
+++ index.js
@@ -0,0 +1,8 @@
+const pizza_flavors = ['Pepperoni', 'Sausage', 'Cheese'];
+
+for (let index = 0; index < pizza_flavors.length; index++) {
+  // console.log(pizza_flavors[index]);
+  console.log(`I like ${pizza_flavors[index]} pizza`);
+}
+
+console.log('I really love pizza!');
