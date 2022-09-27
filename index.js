1260da8c3cb71ed8463e20f4b785f8776f72a827
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 03:47:39 +0500

    exercise 29 completed

---
+++ index.js
@@ -0,0 +1,13 @@
+const favorite_fruits = ['apple', 'mango', 'banana'];
+
+if (favorite_fruits.includes('grapes')) {
+  console.log('I love grapes!');
+} else if (favorite_fruits.includes('apple')) {
+  console.log('I like apples!');
+} else if (favorite_fruits.includes('mango')) {
+  console.log('I like mangoes!');
+} else if (favorite_fruits.includes('banana')) {
+  console.log('I like bananas!');
+} else {
+  console.log('I like none of these fruits!');
+}
