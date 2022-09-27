24986411a3951bbdb29e9db4e61d06cc484a1e03
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-27 23:58:44 +0500

    updated code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //ADDITION
-console.log(5 + 3);
+alert(5 + 3);
 
 //SUBTRACTION
-console.log(11 - 3);
+alert(11 - 3);
 
 //MULTIPLICATION
-console.log(2 * 4);
+alert(2 * 4);
 
 //DIVISION
-console.log(16 / 2);
+alert(16 / 2);
