6ba24fe3f73ba208706b3beca65a57175bd1c80c
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 03:34:05 +0500

    exercise 26 completed

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const alien_color = 'green';
-
-if (alien_color !== 'green') {
-  return;
-}
+// const alien_color = 'yellow';
 
 if (alien_color === 'green') {
   console.log('You earned 5 points!');
+} else {
+  console.log('You earned 10 points!');
 }
