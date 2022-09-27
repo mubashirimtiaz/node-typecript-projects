5d35c5dba230fac67fa48481a4f8af5bf80bd5e2
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 02:38:13 +0500

    exercise 23 completed

---
+++ index.js
@@ -0,0 +1,28 @@
+const name = 'John Doe';
+
+console.log(name === 'John Doe');
+
+const favNumber = 45;
+
+console.log(favNumber === 45);
+
+const num = '5';
+
+console.log(num == 5);
+
+//equality check with type
+console.log(num === 5);
+
+const isCool = true;
+
+console.log(isCool === true ? "I'm cool😎" : "I'm not cool🥲");
+
+console.log(undefined == null);
+
+console.log(undefined === null);
+
+const person = { profession: 'developer', age: 25 };
+
+console.log(person.profession === 'developer');
+
+console.log(person.age === '25');
