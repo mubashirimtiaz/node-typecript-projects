2f03b662a83638cf1a8c68b1c9ef9d8d6b948d08
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-28 05:11:46 +0500

    fixed issue

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const magicians = ['David Copperfield', 'David Blaine', 'Teller'];
 
-magicians.forEach((magician) => {
-  console.log(magician);
-});
+const show_magicians = (magicians) => {
+  magicians.forEach((magician) => {
+    console.log(magician);
+  });
+};
+
+show_magicians(magicians);
